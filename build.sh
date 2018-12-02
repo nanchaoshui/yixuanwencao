1a5c482df25421a13e42e3e708e583486848d87e
chianjin <chian.jin@gmail.com>
2018-12-02 15:01:58 +0800

    修订 build.sh

--- build.sh
+++ build.sh
@@ -13,16 +13,15 @@ fi
 # ------------------------------------------------
 # ->> Get source filename
 # ------------------------------------------------
-FileName=yinxuanwencao
+FileName=`ls *.tex`
 # ------------------------------------------------
 # ->> Build textual content
 # ------------------------------------------------
-$Compiler -synctex=1 -interaction=nonstopmode -output-directory=$BuildPath ${FileName}.tex
-$Compiler -synctex=1 -interaction=nonstopmode -output-directory=$BuildPath ${FileName}.tex
+$Compiler -synctex=1 -interaction=nonstopmode -output-directory=$BuildPath ${FileName}
+$Compiler -synctex=1 -interaction=nonstopmode -output-directory=$BuildPath ${FileName}
 # ------------------------------------------------
 # ->> View compiled file
 # ------------------------------------------------
-start  " " /max "./$BuildPath/${FileName}.pdf"
 echo ------------------------------------------------
-echo $Compiler ${FileName}.tex finished...
+echo $Compiler ${FileName} finished...
 echo ------------------------------------------------
